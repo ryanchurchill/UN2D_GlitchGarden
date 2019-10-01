55b0f1c72ea56759df42af397b1bf22062a1c6de
Ryan Churchill <ryan.churchill@gmail.com>
2019-09-30 21:42:25 -0700

    122 make a splash screen

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadStartMenu()
+    {
+        StartCoroutine(LoadStartMenuWithDelay());
+    }
+
+    private IEnumerator LoadStartMenuWithDelay()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("Start Menu");
+    }
+}
---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<LevelLoader>().LoadStartMenu();
+    }
+}
