44acd87e970200e7027347bcc8e7b49ed7a979d1
Ryan Churchill <ryan.churchill@gmail.com>
2019-10-19 11:53:10 -0700

    135 instantiate from gun position

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectile, gun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, gun.transform.rotation);
+    }
+}
