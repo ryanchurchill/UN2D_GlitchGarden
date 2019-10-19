fde11a285cf7c74cfea1c8df31e565a18d234712
Ryan Churchill <ryan.churchill@gmail.com>
2019-10-19 12:06:07 -0700

    136 making projectile move

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveTock();
+    }
+
+    void moveTock()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+}
