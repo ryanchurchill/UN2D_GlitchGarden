c13f490f146d4715150798bd1c38cbe5a0cc33b1
Ryan Churchill <ryan.churchill@gmail.com>
2019-10-20 14:30:18 -0700

    Fixed trophy animation

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class Defender : MonoBehaviour
 {
     [SerializeField] int starCost = 100;
+
+    public void AddStars(int numStars)
+    {
+        FindObjectOfType<StarDisplay>().AddStars(numStars);
+    }
 }
