1d44f9a3510575ad49a484347c10b73a4a9e503a
Ryan Churchill <ryan.churchill@gmail.com>
2019-10-19 20:25:05 -0700

    139 tidy up and death fx

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] float health = 100f;
+    [SerializeField] GameObject deathVFX;
 
     public void DealDamage(float damage)
     {
         health -= damage;
         if (health <= 0)
         {
+            TriggerDeathVFX();
             Destroy(gameObject);
         }
+    }
+
+    private void TriggerDeathVFX()
+    {
+        if (!deathVFX)
+        {
+            return;
+        }
+
+        var myDeathVFX = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(myDeathVFX, 1);
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -30,6 +30,7 @@ public class Projectile : MonoBehaviour
         if (collidingObjectHealth)
         {
             collidingObjectHealth.DealDamage(damage);
+            Destroy(gameObject);
         }
     }
 }
