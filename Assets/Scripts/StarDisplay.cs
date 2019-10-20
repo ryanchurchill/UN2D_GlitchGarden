82afd16e58901b16341d2b78170c4165906e6426
Ryan Churchill <ryan.churchill@gmail.com>
2019-10-20 13:11:16 -0700

    145 Start Our Resource System

---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarDisplay : MonoBehaviour
+{
+    [SerializeField] int stars = 100;
+
+    // cached components
+    Text starText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        starText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        starText.text = stars.ToString();
+    }
+
+    public void AddStars(int numStars)
+    {
+        stars += numStars;
+        UpdateDisplay();
+    }
+
+    public bool SpendStars(int numStars)
+    {
+        if (numStars > stars)
+        {
+            return false;
+        }
+
+        stars -= numStars;
+        UpdateDisplay();
+
+        return true;
+    }
+}
