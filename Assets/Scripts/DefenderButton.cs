c5713cff74a30b0b7a4fb9bd655542470980278c
Ryan Churchill <ryan.churchill@gmail.com>
2019-10-19 21:28:32 -0700

    143 Create Defender Selection UI

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    [SerializeField]
+    bool isSelected = false;
+
+    private void Start()
+    {
+        updateColorBasedOnIsSelected();
+    }
+
+    private void OnMouseDown()
+    {
+        unselectAllButtons();
+
+        // select this button only
+        isSelected = true;
+        updateColorBasedOnIsSelected();
+    }
+
+    private void unselectAllButtons()
+    {
+        foreach (DefenderButton defenderButton in FindObjectsOfType<DefenderButton>())
+        {
+            defenderButton.unselect();
+        }
+    }
+
+    private void updateColorBasedOnIsSelected()
+    {
+        if (isSelected)
+        {
+            GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = Color.grey;
+        }
+    }
+
+    public void unselect()
+    {
+        isSelected = false;
+        updateColorBasedOnIsSelected();
+    }
+}
