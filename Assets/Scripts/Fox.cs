e1a3b853100edb6cf15d4642b9af64e5825d9757
Ryan Churchill <ryan.churchill@gmail.com>
2019-11-10 15:04:08 -0800

    159 Fantastic Mr. Fox

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    // animator consts
+    const string ANIMATOR_VAR_JUMP = "jumpTrigger";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject otherObject = collision.gameObject;
+
+        Gravestone gravestone = otherObject.GetComponent<Gravestone>();
+        if (gravestone)
+        {
+            GetComponent<Animator>().SetTrigger(ANIMATOR_VAR_JUMP);
+            return;
+        }
+
+        Defender defender = otherObject.GetComponent<Defender>();
+        if (defender)
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+        }
+    }
+}
