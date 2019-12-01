4f74fe0b29e328ce4dec71054ca2dce28086ddc6
Ryan Churchill <ryan.churchill@gmail.com>
2019-12-01 13:19:31 -0800

    164 A Nicer Win Experience

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
+    [SerializeField] GameObject winLabel;
+    [SerializeField] float waitToLoad = 4f;
+    
     int numAttackersOnScreen = 0;
     bool levelTimerFinished = false;
+    bool levelOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        winLabel.SetActive(false);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isLevelOver())
+        if (!levelOver)
         {
-            Debug.Log("End Level Now!");
+            if (isWinCondition())
+            {
+                levelOver = true;
+                //GetComponent<AudioSource>().Play();
+                StartCoroutine(HandleWinCondition());
+            }
         }
+        
+    }
+
+    private IEnumerator HandleWinCondition()
+    {
+        winLabel.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(waitToLoad);
+        FindObjectOfType<LevelLoader>().LoadNextLevel();
     }
 
     public void AttackerSpawned()
@@ -38,7 +57,7 @@ public class LevelController : MonoBehaviour
         StopSpawners();
     }
 
-    private bool isLevelOver()
+    private bool isWinCondition()
     {
         return (numAttackersOnScreen <= 0 && levelTimerFinished);
     }
--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+    const int SPLASH_SCREEN_INDEX = 0;
+    const int START_MENU_INDEX = 1;
+    const int FIRST_LEVEL_INDEX = 2;
+
+    int currentSceneIndex = 0;
+
     public void LoadStartMenu()
     {
         StartCoroutine(LoadStartMenuWithDelay());
@@ -13,12 +19,25 @@ public class LevelLoader : MonoBehaviour
     private IEnumerator LoadStartMenuWithDelay()
     {
         yield return new WaitForSeconds(3);
-        SceneManager.LoadScene("Start Menu");
+        SceneManager.LoadScene(START_MENU_INDEX);
+        currentSceneIndex = START_MENU_INDEX;
     }
 
     public void LoadLoseScreen()
     {
         // TODO: better lose screen
-        SceneManager.LoadScene("Start Menu");
+        SceneManager.LoadScene(START_MENU_INDEX);
+        currentSceneIndex = START_MENU_INDEX;
+    }
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(FIRST_LEVEL_INDEX);
+        currentSceneIndex = FIRST_LEVEL_INDEX;
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 }
