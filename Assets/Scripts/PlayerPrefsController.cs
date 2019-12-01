6da3e2d4ce018155e590571e59c4c9e869703b22
Ryan Churchill <ryan.churchill@gmail.com>
2019-12-01 15:21:26 -0800

    168  PlayerPrefs to save settings

---
+++ Assets/Scripts/PlayerPrefsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsController : MonoBehaviour
+{
+    const string MASTER_VOLUME_KEY = "master_volume";
+    const float MIN_VOLUME = 0f;
+    const float MAX_VOLUME = 1f;
+    const float DEFAULT_VOLUME = MAX_VOLUME;
+
+    const string DIFFICULTY_KEY = "difficulty";
+
+    public static void SetMasterVolume(float volume)
+    {
+        PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, Mathf.Clamp(volume, MIN_VOLUME, MAX_VOLUME));
+    }
+
+    public static void GetMasterVolume()
+    {
+        PlayerPrefs.GetFloat(MASTER_VOLUME_KEY, DEFAULT_VOLUME);
+    }
+}
